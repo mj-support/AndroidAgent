d39e26f275b798d7c4df38c13f9b7910cbf1cda6
mj <51402520+mj-support@users.noreply.github.com>
2025-03-12 18:19:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import gymnasium as gym
 from stable_baselines3 import DQN, PPO
 import environment
 
-def train(env, task):
+def train(env, task, total_timesteps=1000):
     model = DQN("MultiInputPolicy", env=env, verbose=1, learning_rate=0.001, gamma=0.99, exploration_fraction=0.5, seed=42)
     #model = PPO("MultiInputPolicy", env=env, verbose=1, ent_coef=0.1, learning_rate=3e-4, n_steps=20, batch_size=20)
-    model.learn(total_timesteps=7500)
+    model.learn(total_timesteps=total_timesteps)
     model.save(f"models/{task}.zip")
     print(f"Finished training - model saved in models/{task}.zip\n")
 
-def predict(env, task):
+def predict(env, task, total_timesteps=1000):
     model_path = f"models/{task}.zip"
     model = DQN.load(model_path, env=env)
-    model.learn(total_timesteps=1000, reset_num_timesteps=False)
+    model.learn(total_timesteps=total_timesteps, reset_num_timesteps=False)
 
     vec_env = model.get_env()
     obs = vec_env.reset()
@@ -29,10 +29,11 @@ def main():
     env_id = "Android-v0"
     emulator_id = "emulator-5554"   # Default emulator-name
     task = "airplane" #"youtube" #"airplane"
-    env = gym.make(env_id, emulator_id=emulator_id, task=task)
+    exploration_mode = "guided_restricted" # "guided_open", "full_exploration"
+    env = gym.make(env_id, emulator_id=emulator_id, task=task, exploration_mode=exploration_mode, episode_timesteps=100)
 
-    train(env, task)
-    predict(env, task)
+    train(env, task, total_timesteps=1000)
+    predict(env, task, total_timesteps=1000)
 
 if __name__ == "__main__":
     main()
